ca28f362fcc8b9fba3ac42d77b29c4cb2e3ebda3
afk-Marcel <mjgreyling@icloud.com>
2025-07-08 13:11:43 +0200

    Added HomeController

--- grails-app/controllers/expense/sheet/system/HomeController.groovy
+++ grails-app/controllers/expense/sheet/system/HomeController.groovy
@@ -11,4 +11,25 @@ class HomeController {
         }
 
     }
+
+    def setupUser() {
+        [message: "Welcome! Let's set up your account."]
+    }
+
+    def createUser() {
+        def name = params.name
+        def startingBalance = params.startingBalance as BigDecimal
+        def user = new User()
+        user.name = name
+        user.startingBalance = startingBalance
+        user.currentBalance = startingBalance
+
+        if (user.save(flush: true)) {
+            flash.message = "Welcome ${user.name}! Your account is ready."
+            redirect(action: 'index')
+        } else {
+            flash.error = "Please fix the errors and try again."
+            redirect(action: 'setupUser')
+        }
+    }
 }
